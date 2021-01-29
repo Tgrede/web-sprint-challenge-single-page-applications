eb59930bf2bdf2081c10c7dfc6c5f1f21fe745b9
Trenten grede <Trenten.grede@gmail.com>
2021-01-29 14:04:58 -0600

    added change function and form values state to manage changing values

--- src/PizzaCreatePage.js
+++ src/PizzaCreatePage.js
@@ -1,15 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
 import pizza from './Pizza.jpg'
 import styled from 'styled-components'
 import PizzaForm from './PizzaForm'
 
+const initialPizzas = []
+const initialFormValues = {
+     size: '',
+     sauce: 'originalRed',
+     pepperoni: false,
+     sausage: false,
+     canadian_bacon: false,
+     SpicyItalianSausage: false,
+     grilledChicken: false,
+     onions: false,
+     greenPepper: false,
+     dicedTomatos: false,
+     blackOlives: false,
+     roastedGarlic: false,
+     artichokeHearts: false,
+     threeCheese: false,
+     pineapple: false,
+     extraCheese: false,
+     glutenFree: false,
+     quantity: '1',
+}
+
 export default function PizzaCreatePage(props) {
+     const [pizzas, setPizzas] = useState(initialPizzas)
+     const [formValues, setFormValues] = useState(initialFormValues)
+
+     const onChange = (name, value) =>{
+          setFormValues({
+               ...formValues,
+               [name]: value
+          })
+     }
+
      return (
           <StyledPizzaPage>
                <StyledHeader>Build Your Own Pizza</StyledHeader>
                <StyledPizzaPic src={pizza}/>
                <h2>Build Your Own Pizza</h2>
-               <PizzaForm />
+               <PizzaForm values={formValues} change={onChange}/>
           </StyledPizzaPage>
      )
 }
--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -2,17 +2,25 @@ import React from 'react'
 import styled from 'styled-components'
 
 export default function PizzaForm(props) {
+     const {values, change} = props
+
+     const onChange = (e) => {
+          const {name, value, type, checked} = e.target
+          const valueToUse = type === 'checkbox' ? checked : value;
+          change(name, valueToUse)
+     }
+
      return (
           <StyledForm>
 
                <h3>Choose Size</h3>
-               <label>
-                  <select>
-                       <option>--- Select ---</option>
-                       <option>Small (8")</option>
-                       <option>Medium (12")</option>
-                       <option>Large (14")</option>
-                       <option>Extra (18")</option>
+               <label>Size:
+                  <select name='size' onChange={onChange}>
+                       <option value=''>--- Select ---</option>
+                       <option value='small'>Small (8")</option>
+                       <option value='medium'>Medium (12")</option>
+                       <option value='large'>Large (14")</option>
+                       <option value='extra'>Extra (18")</option>
                   </select>
                </label>
 
@@ -20,86 +28,87 @@ export default function PizzaForm(props) {
                <h3>Choose Sauce</h3>
                <StyledLabel>
                     Original Red
-                    <input type='radio' />
+                    <input type='radio' name='sauce' onChange={onChange} value='originalRed' />
                </StyledLabel>
                <StyledLabel>
                     Garlic Ranch
-                    <input type='radio' />
+                    <input type='radio' name='sauce' onChange={onChange} value='garlicRanch' />
                </StyledLabel>
                <StyledLabel>
                     BBQ Sauce
-                    <input type='radio' />
+                    <input type='radio' name='sauce' onChange={onChange} value='bbq' />
                </StyledLabel>
                <StyledLabel>
                     Spinach Alfredo
-                    <input type='radio' />     
+                    <input type='radio' name='sauce' onChange={onChange} value='spinachAlfredo' />     
                </StyledLabel> 
 
                <h3>Choose Toppings</h3>
                <label>
                     Pepperoni
-                    <input type='checkbox'></input>
+                    <input type='checkbox' name='pepperoni' onChange={onChange} checked={values.pepperoni}></input>
                </label>
                <label>
                     Sausage
-                    <input type='checkbox'></input>
+                    <input type='checkbox' name='sausage' onChange={onChange}checked={values.sausage}></input>
                </label>
                <label>
                     Canadian Bacon
-                    <input type='checkbox'></input>
+                    <input type='checkbox' name='canadianBacon' onChange={onChange} checked={values.canadianBacon}></input>
                </label>
                <label>
                     Spicy Italian Sausage
-                    <input type='checkbox'></input>
+                    <input type='checkbox' name='spicyItalianSausage' onChange={onChange} checked={values.spicyItalianSausage}></input>
                </label>
                <label>
                     Grilled Chicken
-                    <input type='checkbox'></input>
+                    <input type='checkbox' name='grilledChicken' onChange={onChange} checked={values.grilledChicken}></input>
                </label>
                <label>
                     Onions
-                    <input type='checkbox'></input>
+                    <input type='checkbox' name='onions' onChange={onChange} checked={values.onions}></input>
                </label>
                <label>Green Pepper
-                    <input type='checkbox'></input>
+                    <input type='checkbox' name='greenPepper' onChange={onChange} checked={values.greenPepper}></input>
                </label>
                <label>Diced Tomatos
-                    <input type='checkbox'></input>
+                    <input type='checkbox' name='dicedTomatos' onChange={onChange} checked={values.dicedTomatos}></input>
                </label>
                <label>Black Olives
-                    <input type='checkbox'></input>
+                    <input type='checkbox' name='blackOlives' onChange={onChange} checked={values.blackOlives}></input>
                </label>
                <label>Roasted Garlic
-                    <input type='checkbox'></input>
+                    <input type='checkbox' name='roastedGarlic' onChange={onChange} checked={values.roastedGarlic}></input>
                </label>
                <label>Artichoke Hearts
-                    <input type='checkbox'></input>
+                    <input type='checkbox' name='artichokeHearts' onChange={onChange} checked={values.artichokeHearts}></input>
                </label>
                <label>Three Cheese
-                    <input type='checkbox'></input>
+                    <input type='checkbox' name='threeCheese' onChange={onChange} checked={values.threeCheese}></input>
                </label>
                <label>Pineapple
-                    <input type='checkbox'></input>
+                    <input type='checkbox' name='pineapple' onChange={onChange} checked={values.pineapple}></input>
                </label>
                <label>Extra Cheese
-                    <input type='checkbox'></input>
+                    <input type='checkbox' name='extraCheese' onChange={onChange} checked={values.extraCheese}></input>
                </label>
 
                <h3>Substitution</h3>
                <label>Gluten Free Crust (+ $1.00)
-                    <input type='checkbox'></input>
+                    <input type='checkbox' name='gluten_free' onChange={onChange}></input>
                </label>
 
                <h3>Special Instructions</h3>
                <label>
-                    <input type='text'></input>
+                    <input type='text' name='special' onChange={onChange}></input>
                </label>
                <div>
-                    <label>#
-                         <input type='number'></input>
+                    <label>
+                         <h3>Add item to order</h3>
+                         <input type='number' name='quantity' onChange={onChange}></input>
                     </label>
 
-                    <button>Submit Order!</button>
+                    <button>Add to Order!</button>
                </div>
           </StyledForm>
      )
